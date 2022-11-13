af84856d8da847ba6397d9a57e0d09159d01cfe9
Florian Buhl <florian.buhl@freenet.de>
2022-11-13 12:01:49 +0100

    Cloned repo needs admin rights

--- src/syncher.py
+++ src/syncher.py
@@ -1,24 +1,37 @@
+import errno
 import os
 import shutil
+import stat
+
 from git import Repo
 
 
-def synch_to_github(git_repo, repo_name, branch, srcDir):
+def handleRemoveReadonly(func, path, exc):
+    excvalue = exc[1]
+    if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
+        os.chmod(path, stat.S_IRWXU| stat.S_IRWXG| stat.S_IRWXO) # 0777
+        func(path)
+    else:
+        raise
+
+def synch_to_github(git_repo, repo_name, branch, src_dir):
     ''' Synchs files to github'''
 
     # clone repository
-    clone_dir = os.path.join(os.path.curdir + repo_name)
-    if os.path.exists(clone_dir) is False:
-        os.mkdir(clone_dir)
-    os.chdir(clone_dir)
+    clone_dir = os.path.join(os.getcwd(), repo_name)
+    print(f"Clone git repo {git_repo} to {clone_dir}")
     repo = Repo.clone_from(git_repo, clone_dir)
+    os.chdir(clone_dir)
 
+    # checkout branch
     if branch not in repo.branches:
-        print("Requested branch not in git repository")
+        # if branch does not exist. create one
+        print("Requested branch not in git repository. Branch created")
         repo.create_head(branch)
-
     repo.git.checkout(branch)
 
-    shutil.rmtree(clone_dir)
-git_repo = "https://github.com/FlorianBuhl/GitCliTest.git"
-synch_to_github(git_repo, "GitCliTest", "main", "D:/Workspaces/syncher/sandbox")
+    # clean up
+    repo.close()
+    shutil.rmtree(os.path.abspath(clone_dir), ignore_errors=False, onerror=handleRemoveReadonly)
+
+synch_to_github("https://github.com/FlorianBuhl/GitCliTest.git", "GitCliTest", "main", "D:/Workspaces/syncher/sandbox")
