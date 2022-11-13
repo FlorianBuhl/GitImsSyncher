4e7dfc26169f2acce9672714b62a1a788936af95
Florian Buhl <florian.buhl@freenet.de>
2022-11-13 12:31:37 +0100

    Untracked file detection added.

--- src/syncher.py
+++ src/syncher.py
@@ -1,26 +1,22 @@
-import errno
+import sys
 import os
-import shutil
-import stat
 
 from git import Repo
 
+SYS_EXIT_UNTRACKED_FILES_IN_TARGET_REPO = "1: Untracked files in target repository"
 
-def handleRemoveReadonly(func, path, exc):
-    excvalue = exc[1]
-    if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
-        os.chmod(path, stat.S_IRWXU| stat.S_IRWXG| stat.S_IRWXO) # 0777
-        func(path)
-    else:
-        raise
 
 def synch_to_github(git_repo, repo_name, branch, src_dir):
     ''' Synchs files to github'''
 
-    # clone repository
     clone_dir = os.path.join(os.getcwd(), repo_name)
-    print(f"Clone git repo {git_repo} to {clone_dir}")
-    repo = Repo.clone_from(git_repo, clone_dir)
+    if os.path.exists(clone_dir):
+        # Folder already exists.
+        repo = Repo(clone_dir)
+    else:
+        # clone repository
+        print(f"Clone git repo {git_repo} to {clone_dir}")
+        repo = Repo.clone_from(git_repo, clone_dir)
     os.chdir(clone_dir)
 
     # checkout branch
@@ -28,10 +24,17 @@ def synch_to_github(git_repo, repo_name, branch, src_dir):
         # if branch does not exist. create one
         print("Requested branch not in git repository. Branch created")
         repo.create_head(branch)
+    print(f"Checkout branch {branch}")
     repo.git.checkout(branch)
+
+    # untracked files would be lost. Terminate with exit code to warn the user.
+    if len(repo.untracked_files) > 0:
+        print("There are untracked files in the repository which would be lost in case the synchronization is executed.")
+        print("Remove them up in front or add them to the repository")
+        print(f"untracked files: {repo.untracked_files}")
+        sys.exit(SYS_EXIT_UNTRACKED_FILES_IN_TARGET_REPO)
 
     # clean up
     repo.close()
-    shutil.rmtree(os.path.abspath(clone_dir), ignore_errors=False, onerror=handleRemoveReadonly)
 
 synch_to_github("https://github.com/FlorianBuhl/GitCliTest.git", "GitCliTest", "main", "D:/Workspaces/syncher/sandbox")
