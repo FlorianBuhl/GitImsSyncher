92638eb3fd224e5d4bf50f73489607d4bd402698
Florian Buhl uidt5911 <florian.buhl@vitesco.com>
2022-12-11 11:18:57 +0100

    read also author from checkpoints

--- src/ims_synch.py
+++ src/ims_synch.py
@@ -4,6 +4,25 @@ import subprocess
 import re
 import shutil
 import os
+import stat
+
+def del_rw(action, name, exc):
+    ''' Removes read protection and removes a file'''
+    os.chmod(name, stat.S_IWRITE)
+    os.remove(name)
+
+def get_from_number(checkpoints: "list[Checkpoint]", number:str):
+    ''' Reports checkpoint from number'''
+    for checkpoint in checkpoints:
+        if checkpoint.number is number:
+            return checkpoint
+    return None
+class Checkpoint:
+    ''' IMS checkpoint class '''
+
+    def __init__(self, number:str, author:str):
+        self.number = number
+        self.author = author
 
 ###################################################################################################
 # get_branches
@@ -34,11 +53,11 @@ def get_branches(ims_project:str) -> list:
 ###################################################################################################
 # get_checkpoints_from
 
-def get_checkpoints_from(ims_project: str, branch:str, lowestCheckpoint: str) -> "list[str]":
+def get_checkpoints_from(ims_project: str, branch:str, lowest_checkpoint_number: str) -> "list[Checkpoint]":
     ''' Reports all checkpoints on a branch coming after the given checkpoint number'''
 
     cmd = f"si viewprojecthistory --project={ims_project} "
-    cmd += "--fields=revision "
+    cmd += "--fields=revision,author "
     if branch == "Normal":
         cmd += "--rfilter=range:1.1-"
     else:
@@ -50,12 +69,25 @@ def get_checkpoints_from(ims_project: str, branch:str, lowestCheckpoint: str) ->
     stdout = result.stdout.decode("utf-8").rstrip()
     print(stdout)
 
-    checkpoints = stdout.split("\n")
-    checkpoints = checkpoints[1:]
-    print(f"checkpoints:{checkpoints}")
+    # each line is representing a checkpoint
+    # the first line holds the bat cmd
+    stdout_lines = stdout.split("\n")
+    stdout_lines = stdout_lines[1:]
+    stdout = "\n".join(stdout_lines)
+    print(f"checkpoints:{stdout_lines}")
 
-    if lowestCheckpoint is not None:
-        idx:int = checkpoints.index(lowestCheckpoint)
+    regex = r"(\S+)\s+(.*)"
+    pattern = re.compile(regex, re.MULTILINE)
+
+    checkpoints = []
+    for match in re.finditer(pattern, stdout):
+        checkpoint = Checkpoint(match.group(1), match.group(2))
+        checkpoints.append(checkpoint)
+
+    # limit list with given lowest checkpoint parameter
+    if lowest_checkpoint_number is not None:
+        lowest_checkpoint = get_from_number(checkpoints, lowest_checkpoint_number)
+        idx:int = checkpoints.index(lowest_checkpoint)
         checkpoints = checkpoints[0:idx]
 
     checkpoints.reverse()
@@ -68,7 +100,7 @@ def get_checkpoints_from(ims_project: str, branch:str, lowestCheckpoint: str) ->
 def checkout(ims_project: str, checkpoint: str, sandbox_dir: str):
     ''' checkout of an specific IMS checkpoint into the given directory '''
 
-    cmd = f"si createsandbox --project={ims_project} --noconfirm -R -Y"
+    cmd = f"si createsandbox --project={ims_project} -R -Y "
     cmd += f"--projectRevision={checkpoint} {sandbox_dir}"
 
     # execute bat cmd
@@ -77,6 +109,8 @@ def checkout(ims_project: str, checkpoint: str, sandbox_dir: str):
         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout = result.stdout.decode("utf-8").rstrip()
     print(stdout)
+    print("files which were checkout")
+    print(os.listdir(sandbox_dir))
 
 ###################################################################################################
 # drop_sandbox
@@ -92,4 +126,13 @@ def drop_sandbox(sandbox_project: str):
     stdout = result.stdout.decode("utf-8").rstrip()
     print(stdout)
 
-    shutil.rmtree(os.path.dirname(sandbox_project))
+    ims_dir = os.path.dirname(sandbox_project)
+
+    # Remove everything in ims_dir folder
+    sub_dirs = os.scandir(ims_dir)
+    for sub_dir in sub_dirs:
+        print(sub_dir)
+        if sub_dir.is_dir():
+            shutil.rmtree(sub_dir.path, onerror=del_rw)
+        elif sub_dir.is_file() or sub_dir.is_symlink():
+            os.unlink(sub_dir.path)
